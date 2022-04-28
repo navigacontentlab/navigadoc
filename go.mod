76a5991bbe68409739f00ba7259e1b52e4fea6c6
Marcus Arnemo <marcus.arnemo@navigaglobal.com>
2022-04-28 13:24:27 +0200

    removed unused dependencies

--- go.mod
+++ go.mod
@@ -3,12 +3,8 @@ module github.com/navigacontentlab/navigadoc
 go 1.17
 
 require (
-	github.com/Infomaker/etree v1.1.0-naviga01
-	github.com/beevik/etree v1.1.0
 	github.com/golang/protobuf v1.5.2
-	github.com/google/go-cmp v0.5.5
 	github.com/google/uuid v1.2.0
-	github.com/microcosm-cc/bluemonday v1.0.2
 	github.com/xeipuuv/gojsonschema v1.2.0
 	google.golang.org/protobuf v1.26.0
 )
@@ -16,5 +12,4 @@ require (
 require (
 	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
 	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
-	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297 // indirect
 )
--- helpers.go
+++ helpers.go
@@ -6,14 +6,12 @@ import (
 
 	"strings"
 
-	"github.com/beevik/etree"
 	"github.com/google/uuid"
 	"github.com/navigacontentlab/navigadoc/doc"
 	"github.com/xeipuuv/gojsonschema"
 )
 
 type BlockVisitor func(block doc.Block, args ...interface{}) (doc.Block, error)
-type ElementVisitor func(element *etree.Element, args ...interface{}) error
 
 // WalkDocument For each Block in the Document call the BlockVisitor functions
 func WalkDocument(document *doc.Document, args []interface{}, fns ...BlockVisitor) error {
@@ -108,111 +106,6 @@ func validateUUID(theUUID string) error {
 	_, err := uuid.Parse(theUUID)
 	if err != nil {
 		return fmt.Errorf("invalid uuid: %v", err)
-	}
-
-	return nil
-}
-
-// Deprecated: UUID handling in OpenContent to be case-insensitive
-// ValidateAndLowercaseNewsMLUUIDs Validate UUID format and convert to lowercase
-// Remove empty uuid attributes from link elements
-func ValidateAndLowercaseNewsMLUUIDs(element *etree.Element, args ...interface{}) error {
-	var err error
-
-	uuidAttr := element.SelectAttr("uuid")
-	// allows for tags in <data> named uuid to be checked
-	if element.Tag != "uuid" && uuidAttr == nil {
-		return nil
-	}
-
-	var theUUID string
-	if uuidAttr != nil {
-		theUUID = uuidAttr.Value
-	} else {
-		theUUID = element.Text()
-	}
-
-	if theUUID != "" {
-		err = ValidateAndLowercaseNewsMLUUID(element, theUUID)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
-}
-
-// ValidateNewsMLUUIDs Validate UUID format, and remove empty uuid attributes from link elements
-func ValidateNewsMLUUIDs(element *etree.Element, args ...interface{}) error {
-	var err error
-
-	uuidAttr := element.SelectAttr("uuid")
-	// allows for tags in <data> named uuid to be checked
-	if element.Tag != "uuid" && uuidAttr == nil {
-		return nil
-	}
-
-	var theUUID string
-	if uuidAttr != nil {
-		theUUID = uuidAttr.Value
-	} else {
-		theUUID = element.Text()
-	}
-
-	if theUUID != "" {
-		err = validateUUID(theUUID)
-		if err != nil {
-			return InvalidArgumentError{
-				Msg: fmt.Sprintf("uuid error %s[%s]: %s", element.GetPath(), theUUID, err),
-			}
-		}
-	}
-
-	return nil
-}
-
-// FixNewsMLNamespaces Add namespace to <links>, <object>, <metadata> and <idf> where missing
-// Returns RequiredArgumentError if link is missing required arguments
-func FixNewsMLNamespaces(element *etree.Element, args ...interface{}) error {
-	switch element.Tag {
-	case "link":
-		uuidAttr := element.SelectAttr("uuid")
-		if uuidAttr != nil && uuidAttr.Value == "" {
-			element.RemoveAttr("uuid")
-		}
-		if rel := element.SelectAttr("rel"); rel == nil {
-			return RequiredArgumentError{
-				Msg: fmt.Sprintf("%s[@rel]", element.GetPath()),
-			}
-		}
-	case "links", "object", "metadata":
-		parent := element.Parent().Tag
-		if parent == "itemMeta" || parent == "contentMeta" {
-			if xmlns := element.SelectAttr("xmlns"); xmlns == nil || xmlns.Value == "" {
-				element.CreateAttr("xmlns", "http://www.infomaker.se/newsml/1.0")
-			}
-		}
-	case "idf":
-		if xmlns := element.SelectAttr("xmlns"); xmlns == nil || xmlns.Value == "" {
-			element.CreateAttr("xmlns", "http://www.infomaker.se/idf/1.0")
-		}
-	}
-	return nil
-}
-
-// Deprecated: UUID handling in OpenContent to be case-insensitive
-// ValidateAndLowercaseNewsMLUUID Validate the UUID format and convert to lowercase
-func ValidateAndLowercaseNewsMLUUID(element *etree.Element, theUUID string) error {
-	err := validateUUID(theUUID)
-	if err != nil {
-		return InvalidArgumentError{
-			Msg: fmt.Sprintf("uuid error %s[%s]: %s", element.GetPath(), theUUID, err),
-		}
-	}
-
-	uuidAttr := element.CreateAttr("uuid", strings.ToLower(theUUID))
-	if uuidAttr == nil {
-		return fmt.Errorf("lowercasing uuid returned nil")
 	}
 
 	return nil
